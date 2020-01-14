72c30fc44e75d1863dd5289e897d9935a32f8e58
Matt Westwick <matt.westwick@gmail.com>
2020-01-13 19:39:47 -0600

    added navbar transition on page 2

--- src/components/MainSideNav.js
+++ src/components/MainSideNav.js
@@ -1,47 +1,99 @@
 import React from 'react';
 import { SideNav, SideNavItem } from 'react-materialize';
+import {Transition} from 'react-transition-group'
 
-const MainSideNav = (props) => (
-    <>
-    <SideNav fixed style={{
-        position : "relative", 
-        padding: 0, 
-        margin : 0
-    }} />
-    <SideNav fixed>
-        <SideNavItem
-            user={{
-                background: 'https://placeimg.com/640/480/tech',
-                email: 'matt@westwick.dev',
-                image: 'static/media/react-materialize-logo.824c6ea3.svg',
-                name: 'Matt Westwick'
-            }}
-            userView
-        />
+const duration = 250;
+const defaultStyle = {
+    transition: `opacity ${duration}ms ease-in-out`,
+    opacity: 0,
+}
+const transitionStyles = {
+    entering: { opacity: 1 },
+    entered:  { opacity: 1 },
+    exiting:  { opacity: 0 },
+    exited:  { opacity: 0 },
+};
 
-        <SideNavItem
-        href="#about"
-        icon="cloud"
-        >
-            About Me
-        </SideNavItem>
+class MainSideNav extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showNav : false
+        }
+    }
 
-        <SideNavItem
-        href="#portfolio"
-        icon="cloud"
-        >
-            Portfolio
-        </SideNavItem>
+    componentDidMount = () => {
+        window.addEventListener('scroll', this.handleScroll);
+    }
+    
+    componentWillUnmount = () => {
+        window.removeEventListener('scroll', this.handleScroll);
+    }
 
-        <SideNavItem
-        href="#contact"
-        icon="cloud"
-        >
-            Contact
-        </SideNavItem>
+    handleScroll = () => {
+        if (parseInt(window.scrollY) > 0.9*parseInt(window.innerHeight)) {
+            this.setState({
+                showNav : true
+            })
+        } else {
+            this.setState({
+                showNav : false
+            })
+        }
+    }
 
-    </SideNav>
-    </>
-)
+    render = () => (
+        <>
+        <SideNav fixed style={{
+            position : "relative", 
+            padding: 0, 
+            margin : 0
+        }} />
+        <Transition in={this.state.showNav} timeout={duration}>
+            {state => (
+                <SideNav 
+                    fixed={true}
+                    style = {{
+                        ...defaultStyle,
+                        ...transitionStyles[state]
+                    }}
+                >
+                    <SideNavItem
+                        user={{
+                            background: 'https://placeimg.com/640/480/tech',
+                            email: 'matt@westwick.dev',
+                            image: 'static/media/react-materialize-logo.824c6ea3.svg',
+                            name: 'Matt Westwick'
+                        }}
+                        userView
+                    />
+            
+                    <SideNavItem
+                    href="#about"
+                    icon="cloud"
+                    >
+                        About Me
+                    </SideNavItem>
+            
+                    <SideNavItem
+                    href="#portfolio"
+                    icon="cloud"
+                    >
+                        Portfolio
+                    </SideNavItem>
+            
+                    <SideNavItem
+                    href="#contact"
+                    icon="cloud"
+                    >
+                        Contact
+                    </SideNavItem>
+            
+                </SideNav>
+            )}
+        </Transition>
+        </>
+    )
+}
 
 export default MainSideNav
