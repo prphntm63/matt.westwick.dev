b5ce3d55a5e660448b33c5a7ffe300818783ad38
Matt Westwick <matt.westwick@gmail.com>
2020-01-16 17:15:37 -0600

    Updates

---
+++ precache-manifest.761d21dade848cf7c1d090610329ae5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d11a27358b9409823cb2495c709df6cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c9ed7c90cd62b145864",
+    "url": "/static/css/main.292c3155.chunk.css"
+  },
+  {
+    "revision": "ef6ae9a796088df41443",
+    "url": "/static/js/2.9b2190fd.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.9b2190fd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2c9ed7c90cd62b145864",
+    "url": "/static/js/main.5d8f51b7.chunk.js"
+  },
+  {
+    "revision": "3292547937bff27a9194",
+    "url": "/static/js/runtime-main.e8023b9d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
